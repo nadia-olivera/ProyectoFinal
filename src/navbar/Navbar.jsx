075b33898459d7975dcaf6e1e97353206d1d429e
Nadia Olivera <nadia.olivera.alzugaray@gmail.com>
2023-09-22 10:27:28 -0300

    registro y login

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -99,3 +99,134 @@ export default function Navbar() {
         </>
     )
 }
+
+
+
+/*
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+//registro
+export default function FormPropsTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          required
+          id="nombre"
+          label=" "
+          defaultValue="Nombre"
+        />
+        <TextField
+          required
+          id="apellido"
+          label=" "
+          defaultValue="Apellido"
+        />
+        <TextField
+          required
+          id="celular"
+          label=" "
+          defaultValue="Celular"
+        />
+        <TextField
+          required
+          id="mail"
+          label=" "
+          defaultValue="Mail"
+        /> 
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+      </div>
+     
+      <Button
+                        sx={{
+                            bgcolor: "black",
+                            color: "white",
+                            gap: 0.5,
+                            borderRadius: 5,
+                            fontWeight: "bold",
+                            textTransform: 'lowercase',
+                            '&:hover': {
+                                bgcolor: "black",
+                                borderColor: 'lime',                                
+                                borderStyle: 'solid',
+                                borderWidth:2
+                                
+                            },
+                        }}>
+                        <AccountCircleOutlinedIcon />
+                        Registrarme
+                    </Button>
+
+    </Box>
+  );
+}
+
+// login
+export default function FormPropsTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+
+        <TextField
+          required
+          id="mail"
+          label=""
+          defaultValue="Mail"
+        />
+      
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+      </div>
+     
+      <Button
+                        sx={{
+                            bgcolor: "black",
+                            color: "white",
+                            gap: 0.5,
+                            borderRadius: 5,
+                            fontWeight: "bold",
+                            textTransform: 'lowercase',
+                            '&:hover': {
+                                bgcolor: "black",
+                                borderColor: 'lime',                                
+                                borderStyle: 'solid',
+                                borderWidth:2
+                                
+                            },
+                        }}>
+                        <AccountCircleOutlinedIcon />
+                        Ingresar
+                    </Button>
+    </Box>
+  );
+}
+
+
+*/
